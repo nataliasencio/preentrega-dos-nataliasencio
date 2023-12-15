536723fc4e6f0a7e20c8fbfebbf1084c097e1910
Natalí Asencio <bebu_7_9@outlook.com>
2023-12-14 23:00:43 -0300

    validar datos al agregar paletas y vaciar resultadoContenedor

--- ultimo.js
+++ ultimo.js
@@ -13,11 +13,11 @@ window.onload = function(){
     }
 }
 //traigo el div por su id donde mostraré los valores de los input en la pagina DOM
-let mostrarResultado = document.getElementById("muestroPaletas")
+let resultadoContenedor = document.getElementById("muestroPaletas")
 
 
 
-//creo un array vacio para agregar los objetos que creo
+//creo un array vacio para agregar los objetos que creo en el evento del boton agregar paleta
 let listaPaletas = []
 
 //le creo un evento al boton para agregar la paleta
@@ -34,30 +34,39 @@ botonAgregarPaleta.addEventListener("click", function(){
         precioPaleta : valorInput3,
         stockPaleta : valorInput4
     }
-    //agrego el objeto a mi array
-    listaPaletas.push(paletaAgregada) 
-    //guardo el array en el localStorage
-    localStorage.setItem("PaletaAgregada", JSON.stringify(listaPaletas))
-    
     //vacio los input luego del click del boton
     nombrePaleta.value = ""
     saborPaleta.value = ""
     precioPaleta.value = ""
     stockPaleta.value = ""
+
+    if(paletaAgregada.nombrePaleta===""||paletaAgregada.saborPaleta===""||paletaAgregada.precioPaleta==="" ||paletaAgregada.stockPaleta ==="" ){
+        alert("Por favor ingresa un valor válido")
+    }else{
+    //agrego el objeto a mi array
+    listaPaletas.push(paletaAgregada) 
+    //guardo el array en el localStorage
+    localStorage.setItem("PaletaAgregada", JSON.stringify(listaPaletas))
     //llamo a la fcion que recorre y muestra el array en el DOM
     mostrarPaletas()
+     
+    }
+    
 })
 
 //creo una fcion que recorre el array y lo muestra en el DOM
 let mostrarPaletas = () =>{
+   
     //vacio los input
+    resultadoContenedor.innerHTML = ""
     nombrePaleta.value = ""
     saborPaleta.value = ""
     precioPaleta.value = ""
     stockPaleta.value = ""
+    
     //recorro el array y lo imprimo en el DOM
     listaPaletas.forEach(function(elemento){
-        mostrarResultado.innerHTML += `Nombre: ${elemento.nombrePaleta}, Sabor: ${elemento.saborPaleta}, Precio: ${elemento.precioPaleta}, Cantidad en stock: ${elemento.stockPaleta}<br/>`
+      resultadoContenedor.innerHTML += `Nombre: ${elemento.nombrePaleta}, Sabor: ${elemento.saborPaleta}, Precio: ${elemento.precioPaleta}, Cantidad en stock: ${elemento.stockPaleta}<br/>`
     })
 
 }
